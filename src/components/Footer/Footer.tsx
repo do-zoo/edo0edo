2d641ed905718625864b58955afba0961448a9d9
do-zoo <edwardedo603@gmail.com>
2024-02-02 03:14:02 +0700

    feat(link): add temporary interaction

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,5 @@
 import { ActionIcon, Container, Group, rem, Text } from '@mantine/core';
-import {
-  IconBrandGithub,
-  IconBrandInstagram,
-  IconBrandLinkedin,
-  IconBrandTwitter,
-  IconBrandYoutube,
-} from '@tabler/icons-react';
+import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-react';
 import { Logo } from '@edo0edo/components';
 import Link from 'next/link';
 import classes from './Footer.module.css';
@@ -34,16 +28,40 @@ export function Footer() {
         <div className={classes.inner}>
           <Text>@ Created by Edwar</Text>
           <Group gap="md" className={classes.links} justify="flex-end" wrap="nowrap">
-            <ActionIcon size="lg" color="gray" variant="outline" radius="xl">
+            <ActionIcon
+              size="lg"
+              color="gray"
+              variant="outline"
+              radius="xl"
+              component={Link}
+              href="https://www.linkedin.com/in/edo0edo/"
+              target="_blank"
+            >
               <IconBrandLinkedin style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
             </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="outline" radius="xl">
+            <ActionIcon
+              size="lg"
+              color="gray"
+              variant="outline"
+              radius="xl"
+              component={Link}
+              href="https://www.instagram.com/ed.0.1/"
+              target="_blank"
+            >
               <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
             </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="outline" radius="xl">
-              <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-            </ActionIcon>
-            <ActionIcon size="lg" color="gray" variant="outline" radius="xl">
+            {/*<ActionIcon size="lg" color="gray" variant="outline" radius="xl">*/}
+            {/*  <IconBrandTwitter style={{ width: rem(18), height: rem(18) }} stroke={1.5} />*/}
+            {/*</ActionIcon>*/}
+            <ActionIcon
+              size="lg"
+              color="gray"
+              variant="outline"
+              radius="xl"
+              component={Link}
+              href="https://github.com/do-zoo"
+              target="_blank"
+            >
               <IconBrandGithub style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
             </ActionIcon>
           </Group>
